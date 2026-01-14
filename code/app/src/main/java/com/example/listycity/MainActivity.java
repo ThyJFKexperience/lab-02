24fc295bbb6bcc6ee2d5c0aabd3ee3066523fd35
ThyJFKexperience <JTensen88@gmail.com>
2026-01-14 16:39:18 -0700

    Finished Code Implementation

--- code/app/src/main/java/com/example/listycity/MainActivity.java
+++ code/app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.listycity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,8 +16,10 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity {
 
     ListView cityList;
+    EditText addCityText;
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
+    int selectedIndex; // The index of the city that has just been clicked on
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         cityList = findViewById(R.id.city_list);
+        addCityText = findViewById(R.id.city_adder);
+
         String []cities = {"Edmonton", "Vancouver", "Moscow", "Sydney", "Berlin", "Vienna", "Tokyo", "Beijing", "Osaka", "New Delhi"};
 
         dataList = new ArrayList<>();
@@ -28,5 +36,39 @@ public class MainActivity extends AppCompatActivity {
 
         cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
         cityList.setAdapter(cityAdapter);
+
+        // Add City Button
+        findViewById(R.id.add_city).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = addCityText.getText().toString();
+                if (!text.isBlank()) {
+                    dataList.add(text);
+                    cityAdapter.notifyDataSetChanged();
+                }
+                else {
+                    Toast emptyToast = Toast.makeText(MainActivity.this, "Please Enter a City Name",
+                            Toast.LENGTH_SHORT);
+                    emptyToast.show();
+                }
+            }
+        });
+
+        // Remove City Button
+        findViewById(R.id.delete_city).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dataList.remove(selectedIndex);
+                cityAdapter.notifyDataSetChanged();
+            }
+        });
+
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectedIndex = position;
+            }
+        });
+
     }
 }
